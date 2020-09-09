94c3fdc5ce7b889fefc24f35db14750d85c5296b
WeAreTheYouth <open8your5mind@gmail.com>
2020-09-10 01:40:06 +0300

    все равно никто не посмотрит

--- gulpfile.js
+++ gulpfile.js
@@ -49,3 +49,127 @@ const watcher = () => {
 exports.default = gulp.series(
   styles, server, watcher
 );
+
+
+////////////////////////////////
+
+// const gulp = require("gulp");
+// const plumber = require("gulp-plumber");
+// const sourcemap = require("gulp-sourcemaps");
+// const sass = require("gulp-sass");
+// const postcss = require("gulp-postcss");
+// const autoprefixer = require("autoprefixer");
+// const csso = require("csso");
+// const rename = require("gulp-rename");
+// const imagemin = require("gulp-imagemin");
+// const sync = require("browser-sync").create();
+// const webp = require("gulp-webp");
+// const rename = require("rename");
+// const svgstore = require("gulp-svgstore");
+
+// // Server
+
+// const server = (done) => {
+//   sync.init({
+//     server: {
+//       baseDir: 'build'
+//     },
+//     cors: true,
+//     notify: false,
+//     ui: false,
+//   });
+//   done();
+// }
+
+// // Watcher
+
+// const watcher = () => {
+//   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
+//   gulp.watch("source/*.html", gulp.series("html"));
+// }
+
+// // Styles
+
+// const styles = () => {
+//   return gulp.src("source/sass/style.scss")
+//     .pipe(plumber())
+//     .pipe(sourcemap.init())
+//     .pipe(sass())
+//     .pipe(postcss([
+//        autoprefixer()
+//      ]))
+//     .pipe(csso())
+//     .pipe(rename("styles.min.css"))
+//     .pipe(sourcemap.write("."))
+//     .pipe(gulp.dest("build/css"))
+//     .pipe(sync.stream());
+// }
+// exports.styles = styles;
+
+// // Images
+
+// const images = () => {
+//     return gulp.src("source/img/**/*.{jpg,png,svg}")
+//         .pipe(imagemin([
+//             imagemin.optipng({optimizationLevel: 3}),
+//             imagemin.jpegtran({progressive: true}),
+//             imagemin.svgo()
+//         ])) 
+// }
+// exports.images = images;
+
+// // convert WebP
+
+// const createWebp = () => {
+//     return gulp.src("source/img/**/*.{png,jpg}")
+//         .pipe(webp({quality: 90}))
+//         .pipe(gulp.dest("source/img"))
+// }
+// exports.webp = createWebp;
+
+// // Sprite
+
+// const sprite = () => {
+//     return gulp.src("source/img/**/icon-*.svg")
+//         .pipe(svgstore())
+//         .pipe(rename("sprite.svg"))
+//         .pipe(gulp.dest("build/img"))
+// }
+// exports.sprite = sprite;
+
+// // Copy
+
+// const copy = () => {
+//   return gulp.src([
+//       "source/fonts/**/*.{woff,woff2}",
+//       "source/img/**",
+//       "source/js/**",
+//       "source/*.ico"
+//   ], {
+//       base: "source"
+//   })
+//   .pipe(gulp.dest(“build"));
+// };
+// exports.copy = copy;
+
+// // Clean
+
+// const del = require("del");
+
+// const clean = () => {
+//     return del("build");
+// };
+// exports.clean = clean;
+
+// // Build
+
+// const gulp = require("gulp");
+
+// const build = gulp.series(
+//     "clean",
+//     "copy",
+//     "css",
+//     "sprite",
+//     "html"
+// );
+// exports.build = build;
